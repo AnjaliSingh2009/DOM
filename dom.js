7909ea5f92682ca68a16fe41c1e7fbb7a0f4924d
AnjaliSingh2009 <kumarianjali05102009@gmail.com>
2024-09-15 22:09:54 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,31 @@
+function validate(e) {
+    e.preventDefault()
+
+
+    const email = document.getElementById('email').value;
+    const pass = document.getElementById('password').value;
+    const age = document.getElementById('age').value;
+    const msgBox = document.getElementById('message');
+
+
+    let message = ' ';
+
+    if (email === ' '){
+        message = 'enter an email.';
+        msgBox.style.color = 'red';
+    }
+     else if (pass === ' '){
+        message = 'enter a password.';
+        msgBox.style.color = 'red';
+    }
+
+    else if (age === ' '){
+        message = 'enter your age.';
+        msgBox.style.color = 'red';
+    }
+      else{
+    message = 'Login Successful!';
+    msgBox.style.color = 'green';
+    }
+msgBox.innerText = message;
+}
